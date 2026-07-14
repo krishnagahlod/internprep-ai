ccb4846ebd553bdf87d8de144ce0ef4784966384
krishnagahlod <krishnagahlod@gmail.com>
2026-07-14 11:29:21 +0530

    fix: remove unsupported sentry v8 options from next.config.ts

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -20,16 +20,7 @@ export default withSentryConfig(nextConfig, {
   // Upload a larger set of source maps for prettier stack traces (increases build time)
   widenClientFileUpload: true,
 
-  // Transpiles SDK to be compatible with IE11 (increases bundle size)
-  transpileClientSDK: true,
-
   // Routes browser requests to Sentry through a Next.js rewrite to circumvent ad-blockers.
   // This can increase your server load as well as your hosting bill.
   tunnelRoute: "/monitoring",
-
-  // Hides source maps from generated client bundles
-  hideSourceMaps: true,
-
-  // Automatically tree-shake Sentry logger statements to reduce bundle size
-  disableLogger: true,
 });
